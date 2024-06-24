767daf5a40219fc8ebd555099ce48a79fd760eb7
Crowdbotics <team@crowdbotics.com>
2024-06-24 03:53:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tunestream-48525",
+  url: "https://tunestream-48525.botics.co",
+  api: "https://tunestream-48525.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
